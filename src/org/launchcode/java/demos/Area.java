62a64b11b4c600e34ad6e71c2300a36184f77470
JD Sareault <jdsareault@gmail.com>
2017-03-02 20:23:55 -0500

    Initial commit for Area studio

---
+++ src/org/launchcode/java/demos/Area.java
@@ -0,0 +1,24 @@
+package org.launchcode.java.demos;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+/**
+ * Created by jd on 3/2/17.
+ * Takes a radius as input and outputs the area.
+ */
+public class Area {
+    public static void main(String[] args) {
+        double area;
+        double radius;
+        Scanner in;
+
+        in = new Scanner(System.in);
+        System.out.println("");
+        radius = in.nextDouble();
+
+        area = Math.PI * radius * radius;
+
+        System.out.println("The area is " + area);
+    }
+}
